bc9c8c7c0aa13baa314ebb0ee7de4d849b19ddce
Douglas Magalhães <douglas_edbr@hotmail.com>
2021-04-24 14:17:45 -0300

    Inserindo o exercício de operações Matemáticas

---
+++ Math_Operations/Match_Operations.py
@@ -0,0 +1,16 @@
+#OPERAÇÔES MATEMÁTICAS
+
+a = 5
+b = 3
+print(a, b)
+
+a + b
+print('A soma é' , a + b)
+print('A subtração é', a - b)
+print('A multiplicação é', a * b)
+print('A divisão é', a / b)
+print('O resto da divisão é', 10 % 3)
+print('5 elevado a 4 é', 5**4)
+
+import math     #IMPORT MATH - Importar funções matemáticas já pre-definadas no python.
+math.sqrt(81)   #SQRT - Reter, significa raiz quadrada

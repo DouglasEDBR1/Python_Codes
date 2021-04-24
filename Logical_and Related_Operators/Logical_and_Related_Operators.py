68f520ee56486c4005d1914384e7bafe01e34c0d
Douglas Magalhães <douglas_edbr@hotmail.com>
2021-04-24 14:30:09 -0300

    Inserindo exercício operadores lógicos e relacionados

---
+++ Logical_and Related_Operators/Logical_and_Related_Operators.py
@@ -0,0 +1,26 @@
+#Operadores Lógicos e Relacionais
+
+#Operadores Lógicos
+
+a = True
+b = False
+
+print(a, b)
+
+a and b  #Os dois precisariam ser verdadeiro para retornar True, como o 'b' é falso, ele retornará False.
+a & b    #Outra opçao do mesmo código
+
+c = a and b
+print("'A' e 'B' são iguais é", c)
+
+#a or b  - Retornará True, caso uma dessa variáveis seja verdadeiro, ele retornará True.
+
+a | b
+
+d = a or b
+print ("'a' ou 'b' é igual a", d)
+
+#not a  - Operador Not é igual a Negação, ele retorna o valor ao contrário.
+
+#Operadores Relacionais
+
